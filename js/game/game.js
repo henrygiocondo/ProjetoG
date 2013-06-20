6e6aa39d5f83d6ee840ff738a08c4e5d6bb832bb
Denilson Filho <dpf.denilson@gmail.com>
2013-06-20 01:48:49 -0300

    ProjetoG 0.0.1c
    Melhorias gerais, troca da versão do prototype

--- js/game/game.js
+++ js/game/game.js
@@ -1,40 +1,63 @@
 var Game = Class.create({
-    initialize: function (context) {
+    initialize: function (context, config) {
         this.context = context;    
         this.fixDef = new b2FixtureDef;
         this.bodyDef = new b2BodyDef;
         this.scale = 30;
-        this.world = this.createWorld();        
+        this.world = this.createWorld();
+        this.players = [];
+		this.width = ("width" in config)?(config.width):(2000);
+		this.height = ("height" in config)?(config.height):(400);
+		
+		this.offsetX = 0;
+		this.offsetY = 0;
+		this.rotation = 0;
+		this.objectOfInterest = null;
     },
     createWorld: function () {        
         return new b2World(new b2Vec2(0, 10), true);
     },
-    createGround: function () {      
+    createGround: function () {
+
         this.fixDef.density = 1.0;
         this.fixDef.friction = 0.5;
         this.fixDef.restitution = 0.2;
+
+        this.fixDef.filter.categoryBits = 0x0004;
+        this.fixDef.filter.maskBits = 0x0002;
+
         this.bodyDef.type = b2Body.b2_staticBody;
         this.fixDef.shape = new b2PolygonShape;
-        this.fixDef.shape.SetAsBox(20, 2);
-        this.bodyDef.position.Set(10, 400 / this.scale + 1.8);
+        this.fixDef.shape.SetAsBox(this.width / this.scale, 0.1);
+        this.bodyDef.position.Set(0, this.height / this.scale);
         this.world.CreateBody(this.bodyDef).CreateFixture(this.fixDef);
-        this.bodyDef.position.Set(10, -1.8);
-        this.world.CreateBody(this.bodyDef).CreateFixture(this.fixDef);
-        this.fixDef.shape.SetAsBox(2, 14);
-        this.bodyDef.position.Set(-1.8, 13);
-        this.world.CreateBody(this.bodyDef).CreateFixture(this.fixDef);
-        this.bodyDef.position.Set(21.8, 13);
-        this.world.CreateBody(this.bodyDef).CreateFixture(this.fixDef);
+        //this.bodyDef.position.Set(10, -1.8);
+        //this.world.CreateBody(this.bodyDef).CreateFixture(this.fixDef);
+        //this.fixDef.shape.SetAsBox(2, 14);
+        //this.bodyDef.position.Set(-1.8, 13);
+        //this.world.CreateBody(this.bodyDef).CreateFixture(this.fixDef);
+        //this.bodyDef.position.Set(21.8, 13);
+        //this.world.CreateBody(this.bodyDef).CreateFixture(this.fixDef);
     },
     createPlayer: function (player) {
                
         this.fixDef.shape = new b2PolygonShape;
         this.fixDef.shape.SetAsBox(player.width / this.scale, player.height / this.scale);
-               
+        this.fixDef.density = 1;
+
+        this.fixDef.filter.categoryBits = 0x0002;
+        this.fixDef.filter.maskBits = 0x0004;
+
         this.bodyDef.type = b2Body.b2_dynamicBody;
+        this.bodyDef.isBullet = true;
         this.bodyDef.position.x = player.x / this.scale;
         this.bodyDef.position.y = player.y / this.scale;
-        this.world.CreateBody(this.bodyDef).CreateFixture(this.fixDef);
+        
+        var body = this.world.CreateBody(this.bodyDef);
+        body.CreateFixture(this.fixDef);
+
+        player.body = body;
+        this.players.push(body);
 
     },
     debugDraw: function () {
@@ -46,9 +69,29 @@ var Game = Class.create({
         debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
         this.world.SetDebugDraw(debugDraw);
     },
+	setInterest: function (obj) {
+		this.objectOfInterest = obj;
+	},
     draw: function () {
         this.world.Step(1 / 60, 10, 10);
         this.world.DrawDebugData();
+
+		var x = 0;
+		var y = 0;
+
+		if (this.objectOfInterest != null) {
+			x = this.objectOfInterest.body.GetLocalCenter().x - this.offsetX;
+			y = this.objectOfInterest.body.GetLocalCenter().y - this.offsetY;
+			this.offsetX = this.objectOfInterest.body.GetLocalCenter().x;
+			this.offsetY = this.objectOfInterest.body.GetLocalCenter().y;
+		}
+		// Implementar centralização da tela
+		//this.context.save();
+		//this.context.translate(x, y);
+		//this.context.rotate(this.rotation);
+		//this.context.drawImage()
+		//this.context.restore();
+
         this.world.ClearForces();
     },
     start: function () {
--- js/game/player.js
+++ js/game/player.js
@@ -6,9 +6,26 @@ var Player = Class.create({
         this.height = 30;
         this.y = y;
         this.x = x;
+        this.body = null;
 
     },
     say: function (message) {
         return this.name + ': ' + message;
-    },   
+    },
+    move: function (direction) {
+
+        var vel = this.body.GetLinearVelocity();
+        switch ( direction )
+        {
+            case 'Left': vel.x = -2; break;
+            case 'Right': vel.x = 2; break;
+            default: vel.x = 0; break;
+        }
+
+        if (vel.x != 0) {
+            this.body.SetAwake(true);
+        }
+
+        this.body.SetLinearVelocity(vel);
+    },
 });
